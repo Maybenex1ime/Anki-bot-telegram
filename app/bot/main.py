f2b72f070351291cad3b04c746d9f5345672ba7a
doman <domanhitmo@gmail.com>
2026-07-22 19:11:47 +0700

    feat: in-chat review session with SM-2 rating buttons

--- app/bot/main.py
+++ app/bot/main.py
@@ -5,7 +5,7 @@ from telegram.ext import (Application, CallbackQueryHandler, CommandHandler,
                           MessageHandler, filters)
 
 from app import config, db, lookup
-from app.bot import create_flow, misc, textrouter
+from app.bot import create_flow, misc, review_flow, textrouter
 from app.bot.auth import owner_filter
 from app.bot.textrouter import register
 
@@ -27,7 +27,9 @@ def build_app() -> Application:
     app = (Application.builder().token(config.BOT_TOKEN)
            .post_init(post_init).build())
     app.add_handler(CommandHandler("start", misc.cmd_start, filters=owner_filter))
+    app.add_handler(CommandHandler("on", review_flow.cmd_review, filters=owner_filter))
     app.add_handler(CallbackQueryHandler(create_flow.on_callback, pattern=r"^pc_"))
+    app.add_handler(CallbackQueryHandler(review_flow.on_callback, pattern=r"^rv_"))
     app.add_handler(MessageHandler(owner_filter & filters.TEXT & ~filters.COMMAND, textrouter.on_text))
     app.add_handler(MessageHandler(owner_filter & filters.PHOTO, textrouter.on_photo))
     return app
